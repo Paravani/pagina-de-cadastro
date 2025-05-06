b7859f8534742d2cd91826b2f563ab672c038e3b
Paravani <mparavani6@gmail.com>
2025-05-06 20:03:54 -0300

    att

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 const createWindow = () => {
@@ -6,12 +6,18 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true, 
+      contextIsolation: false,
     }
   })
-  win.loadFile('index.html')
+    win.loadFile('index.html');
 }
+
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
   createWindow()
+})
+
+app.on('window-all-clossed', ()=> {
+    if(process.platform !== 'darwin') app.quit()
 })
